14a51cec99132881d78e8bebe65404671eb72784
Clément Nerma <clement.nerma@gmail.com>
2023-10-17 11:50:38 +0200

    Fix: command-line flag

--- src/cmd.rs
+++ src/cmd.rs
@@ -137,7 +137,7 @@ pub struct UpdateArgs {
     #[clap(help = "List of packages to update (all if none provided)")]
     pub names: Vec<String>,
 
-    #[clap(help = "Force update even if no update was detected")]
+    #[clap(short, long, help = "Force update even if no update was detected")]
     pub force: bool,
 }
 
